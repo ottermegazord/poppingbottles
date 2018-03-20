c1aa4a56de75065bda1e4074207b982728d69040
ottermegazord <idaly.ali@live.fr>
2018-03-20 16:49:16 -0400

    adds mser written with python

---
+++ CNN/imageSeg.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+import cv2 as cv
+import sys
+
+img = cv2.imread('test_input.tif', 0)
+img_color = cv2.imread('test_input.tif', cv2.IMREAD_COLOR)
+mser = cv2.MSER_create()
+vis = img.copy()
+regions, bboxes = mser.detectRegions(img)
+
+hulls = [cv.convexHull(p.reshape(-1, 1, 2)) for p in regions]
+cv.polylines(vis, hulls, 1, (0, 255, 0))
+
+mask = np.zeros((img.shape[0], img.shape[1], 1), dtype=np.uint8)
+mask = cv2.dilate(mask, np.ones((150, 150), np.uint8))
+for contour in hulls:
+    cv2.drawContours(mask, [contour], -1, (255, 255, 255), -1)
+
+    text_only = cv2.bitwise_and(img, img, mask=mask)
+
+for i, contour in enumerate(hulls):
+    x,y,w,h = cv2.boundingRect(contour)
+    cv2.imwrite('{}.png'.format(i), img_color[y:y+h,x:x+w])
+
+cv2.imshow('img', vis)
+cv2.waitKey(0)
